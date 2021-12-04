372310501a01bfd0612f7563e08b637e3974d957
aadityabhusal <bhusal.001aditya@gmail.com>
2021-12-04 22:36:17 +0545

    feature: create initial logic and mock data for account balances endpoint

--- lib/teller/account/balances.ex
+++ lib/teller/account/balances.ex
@@ -1,5 +1,20 @@
 defmodule TellerWeb.Account.Balances do
+  @api_url "http://localhost:4000"
+
   def get_balances do
-    %{data: "get_balances"}
+    account_id = "acc_nmfff743stmo5n80t4000"
+    {available, ledger} = {33648.09, 33803.48}
+
+    balances = %{
+      account_id: account_id,
+      available: available,
+      ledger: ledger,
+      links: %{
+        account: "#{@api_url}/accounts/#{account_id}",
+        self: "#{@api_url}/accounts/#{account_id}/balances"
+      }
+    }
+
+    balances
   end
 end
