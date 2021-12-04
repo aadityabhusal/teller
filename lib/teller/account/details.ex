187109121092686e6f63fff19685afbc543e050f
aadityabhusal <bhusal.001aditya@gmail.com>
2021-12-04 22:31:31 +0545

    feature: create initial logic and mock data for account details endpoint

--- lib/teller/account/details.ex
+++ lib/teller/account/details.ex
@@ -1,5 +1,23 @@
 defmodule TellerWeb.Account.Details do
+  @api_url "http://localhost:4000"
+
   def get_details do
-    %{data: "get_details"}
+    account_id = "acc_nmfff743stmo5n80t4000"
+    account_number = "891824333836"
+    routing_number = "581559698"
+
+    details = %{
+      account_id: account_id,
+      account_number: account_number,
+      links: %{
+        account: "#{@api_url}/accounts/#{account_id}",
+        self: "#{@api_url}/accounts/#{account_id}/details"
+      },
+      routing_numbers: %{
+        ach: routing_number
+      }
+    }
+
+    details
   end
 end
