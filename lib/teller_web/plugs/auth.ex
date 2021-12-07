10683b86c3726a9e22e91fda2663b0f55265f317
aadityabhusal <bhusal.001aditya@gmail.com>
2021-12-07 13:39:01 +0545

    feature: adds auth logic to get accout token number

--- lib/teller_web/plugs/auth.ex
+++ lib/teller_web/plugs/auth.ex
@@ -4,9 +4,10 @@ defmodule TellerWeb.Plugs.Auth do
   def init(default), do: default
 
   def call(conn, _default) do
-    with {"test_" <> username, _password} <- Plug.BasicAuth.parse_basic_auth(conn) do
-      # Create token number here
-      assign(conn, :temp, username)
+    with {username, _password} <- Plug.BasicAuth.parse_basic_auth(conn) do
+      {:ok, "test_" <> decoded} = Base.decode64(username)
+      {token, _} = decoded |> Base.encode16() |> String.split_at(21)
+      assign(conn, :token, String.to_integer(token))
     else
       _ -> conn |> Plug.BasicAuth.request_basic_auth() |> halt()
     end
