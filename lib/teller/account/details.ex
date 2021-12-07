931f772cede2174bc372808280d372f8bf67bcfa
aadityabhusal <bhusal.001aditya@gmail.com>
2021-12-07 16:05:38 +0545

    refactor: make routing number dynamic

--- lib/teller/account/details.ex
+++ lib/teller/account/details.ex
@@ -2,16 +2,19 @@ defmodule TellerWeb.Account.Details do
   @api_url "http://localhost:4000"
 
   def get_details(number, account_id) do
+    account_number = String.slice("#{number}", -4..-1)
+    routing_number = Base.encode16("#{account_number}")
+
     if account_id === "acc_#{number}" do
       %{
         account_id: account_id,
-        account_number: String.slice("#{number}", -4..-1),
+        account_number: account_number,
         links: %{
           account: "#{@api_url}/accounts/#{account_id}",
           self: "#{@api_url}/accounts/#{account_id}/details"
         },
         routing_numbers: %{
-          ach: "acc_#{number}"
+          ach: routing_number
         }
       }
     else
